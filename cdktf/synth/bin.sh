4d0339addc41fdfe8a5e6e5b40e006c893a9bbe8
Paul Draper <paulddraper@gmail.com>
2023-08-07 13:47:06 -0600

    Make CDKTF synth quiet

--- cdktf/synth/bin.sh
+++ cdktf/synth/bin.sh
@@ -4,4 +4,4 @@ export -n RUNFILES_DIR
 export -n RUNFILES_MANIFEST_FILE
 
 # would be nice to hide "Generated Terraform code" output
-exec "$1" synth -o "$dir"/"$2" >&2
+exec "$1" synth -o "$dir"/"$2" --quiet >&2
